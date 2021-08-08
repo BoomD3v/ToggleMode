d4f229a363f10771c54a614278ba9ceabeb49b36
BoomDev <willferrens@gmail.com>
2021-08-08 02:40:24 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const { Plugin } = require('powercord/entities');
+const { Tooltip } = require('powercord/components');
+const { inject, uninject } = require('powercord/injector');
+const { React, getModule, getModuleByDisplayName } = require('powercord/webpack');
+
+const ToggleButton = require('./components/icons/NotebookButton')
+
+module.exports = class ToggleMode extends Plugin {
+    async startPlugin() {
+        this._injectHeaderBarContainer();
+
+        this.loadStylesheet('styles.scss');
+    }
+
+    async _injectHeaderBarContainer() {
+        const HeaderBarContainer = await getModuleByDisplayName('HeaderBarContainer');
+        const classes = await getModule(['iconWrapper', 'clickable']);
+
+        inject('toggle-header-bar', HeaderBarContainer.prototype, 'render', (args, res) => {
+            res.props.toolbar.props.children.push(
+                React.createElement(Tooltip, {
+                    text: 'Toggle', position: 'bottom'
+                }), React.createElement('div', {
+                    className: `note-button ${classes.iconWrapper} ${classes.clickable}`
+                }), React.createElement(ToggleButton, {
+                    className: `note-button ${classes.icon}`,
+                    onClick: () => {
+                        return;
+                    }
+                })
+            );
+        });
+    }
+}

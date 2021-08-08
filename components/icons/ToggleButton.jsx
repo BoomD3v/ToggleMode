5397bd3d52b28a8aad5ec843fee228fa139a1dfa
BoomDev <willferrens@gmail.com>
2021-08-08 02:40:41 -0400

    Add files via upload

--- components/icons/ToggleButton.jsx
+++ components/icons/ToggleButton.jsx
@@ -1,1 +1,14 @@
+const { React } = require('powercord/webpack')
 
+module.exports = React.memo(
+  (props) => (
+    <svg viewBox='6 3.7 16 16' width={24} height={24} {...props}>
+      <path
+        fill='currentColor'
+        d='M 12.5,3.5
+        a 22.5,22.5 0 0,1 0,43
+        a 22.5,22.5 0 1,0 0,-43 z'
+      />
+    </svg>
+  )
+)
